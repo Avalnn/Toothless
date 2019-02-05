2d66d697c5ca767e9bae65c2d06d8eb643292465
Avalnn <beearrable@gmail.com>
2019-02-05 17:48:02 +0000

    Added Join EventListener

---
+++ src/main/java/kyle/saishu/events/Join.java
@@ -0,0 +1,19 @@
+package kyle.saishu.events;
+
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class Join extends ListenerAdapter {
+
+	public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+
+		EmbedBuilder join = new EmbedBuilder();
+		join.setColor(0xce8de8);
+		join.setAuthor(event.getMember().getEffectiveName(), null, event.getMember().getUser().getAvatarUrl());
+		join.setDescription(event.getMember().getAsMention() + " has joined!");
+
+		event.getGuild().getTextChannelById("541407727319121930").sendMessage(join.build()).queue();
+
+	}
+}
